434c3ddc640efe0b23ac1ce28ce2d6b5ea34a74b
Ebert Matthee <ebert@ecmatthee.com>
2025-03-20 01:17:13 +0200

    Initial Commit

---
+++ flake.nix
@@ -0,0 +1,38 @@
+{
+  description = "Flake";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = inputs@{ nixpkgs,  ... }:
+    let
+      forAllSystems = function:
+        nixpkgs.lib.genAttrs [
+          "aarch64-linux"
+          "aarch64-darwin"
+          "i686-linux"
+          "x86_64-darwin"
+          "x86_64-linux"
+        ] (system: function nixpkgs.legacyPackages.${system});
+
+      devDeps = (with nixpkgs.pkgs; [
+      ]);
+      buildDeps = (with nixpkgs.pkgs; [
+      ]);
+      runtimeDeps = (with nixpkgs.pkgs; [
+      ]);
+    in
+    {
+      nixosModules = {
+        wrapper = ./modules/sops;
+        default = self.nixosModules.wrapper;
+      };
+      devShells = forAllSystems (pkgs: {
+        default = pkgs.mkShell {
+          packages = devDeps ++ buildDeps ++ runtimeDeps;
+          LD_LIBRARY_PATH = "${pkgs.lib.makeLibraryPath runtimeDeps}";
+        };
+      });
+    };
+}
---
+++ modules/wrapper/default.nix
@@ -0,0 +1,68 @@
+{
+  lib
+  pkgs
+}:
+
+let
+  wrapperOptions = {config}: {
+    options = {
+      basePackage = lib.mkOption {
+        type = with lib.types; package;
+        description =  ''
+          Program to be wrapped.
+        '';
+        example = lib.literalExpression "pkgs.nix";
+      };
+
+      extraPackages = lib.mkOption {
+        type = with lib.types; listOf package;
+        default = [];
+        description = ''
+          Extra packages to also wrap.
+        '';
+        example = lib.literalExpression "[ pkgs.git-extras pkgs.delta ]";
+
+      };
+      #environment = {};
+      #wrapperFlags = {};
+      #postBuild = {};
+      #pathAdd = {};
+
+      wrapped = mkOption {
+        type = with lib.types; package;
+        readOnly = true;
+        description = ''
+          (Output) Final wrapped package.
+        '';
+      };
+    };
+
+    config = {
+      wrapped = pkgs.symlinkJoin {
+        paths = [ config.basePackage ] ++ config.extraPackages;
+	buildInputs = [ pkgs.makeWrapper ];#Native?
+	postBuild = ''
+	  for file in "$out"/bin/*; do
+	    echo "Wrapping $file"
+	    wrapProgram \
+	      "$file"
+	  done
+        '';
+      };
+    };
+  }
+in
+{
+  options = {
+    wrappers = mkOption {
+      type = with types; attrsOf (submodule wrapperOptions);
+      default = {};
+      description = ''
+        Wrapper configuration. See the suboptions for configuration.
+      '';
+    };
+  };
+
+  config = {
+  };
+}
